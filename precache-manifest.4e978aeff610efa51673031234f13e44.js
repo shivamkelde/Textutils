191bf849f3d9854e509b2615599e5d2e5dd93a35
shivam kelde <kelde250@gmail.com>
2024-07-25 09:16:39 +0530

    Updates

---
+++ precache-manifest.4e978aeff610efa51673031234f13e44.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e0e4c82b20227a4072db7977e3b26bff",
+    "url": "/index.html"
+  },
+  {
+    "revision": "33ef266a565c94b6a680",
+    "url": "/static/css/2.98036eff.chunk.css"
+  },
+  {
+    "revision": "a7d0dc25548c90f4ead2",
+    "url": "/static/css/main.b53c536b.chunk.css"
+  },
+  {
+    "revision": "33ef266a565c94b6a680",
+    "url": "/static/js/2.bb1c2574.chunk.js"
+  },
+  {
+    "revision": "8bf7614a6420c3456690",
+    "url": "/static/js/3.2d98378e.chunk.js"
+  },
+  {
+    "revision": "a7d0dc25548c90f4ead2",
+    "url": "/static/js/main.0f79591c.chunk.js"
+  },
+  {
+    "revision": "11daa84bf1169ffba932",
+    "url": "/static/js/runtime~main.a8504550.js"
+  }
+]);
